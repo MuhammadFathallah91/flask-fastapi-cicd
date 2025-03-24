ff1ec4facf13a32596f5faec1e1f38c9d4693df2
dola <mohammedfathallah91@gmail.com>
2025-03-24 11:36:30 +0300

    second commit

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from flask import Flask
-from fastapi import FastAPI
-from mangum import Mangum
 
-flask_app = Flask(__name__)
-fastapi_app = FastAPI()
+app = Flask(__name__)
 
-@flask_app.route("/")
-def hello_flask():
+@app.route("/")
+def home():
     return "Hello from Flask!"
 
-@fastapi_app.get("/fastapi")
-async def hello_fastapi():
-    return {"message": "Hello from FastAPI!"}
-
-# Wrap FastAPI with Mangum to run on WSGI server
-handler = Mangum(fastapi_app)
-
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
